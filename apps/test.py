a69e601412271ebe7d50b7246e8e3aed491cacc1
Zedan Liu <zedan.liu@icloud.com>
2025-05-09 19:06:21 -0400

    modified:   apps/test.py

--- apps/test.py
+++ apps/test.py
@@ -164,5 +164,205 @@ def _(mo):
     return
 
 
+app._unparsable_cell(
+    r"""
+    ## Section 0.7
+
+    ### Exercise 0
+
+    **Recalling that $\begin{pmatrix} n \\ k \end{pmatrix} \overset{\text{def}}{=} \frac{n!}{k!(n - k)!}$, prove that for any integers $n \ge k \ge 1$ one has: $\begin{pmatrix} n + 1 \\ k \end{pmatrix} = \begin{pmatrix} n \\ k \end{pmatrix} + \begin{pmatrix} n \\ k-1 \end{pmatrix}$**.
+
+    Proof
+
+    We have $$\begin{aligned}\begin{pmatrix} n + 1 \\ k \end{pmatrix} &\overset{\text{def}}{=} \frac{(n + 1)!}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1)}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 + k - k)}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k) + (n!)k}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k)}{k!(n + 1 - k)!} + \frac{(n)!k}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k)}{k!(n - k)!(n + 1 - k)} + \frac{(n)!k}{(k - 1)!(n - k + 1)!k} \\ &= \frac{(n)!}{k!(n - k)!} + \frac{(n)!}{(k - 1)!(n - k + 1)!} \\ &\overset{\text{def}}{=} \begin{pmatrix} n \\ k \end{pmatrix} + \begin{pmatrix} n \\ k-1 \end{pmatrix}.\end{aligned}$$
+
+    ### Exercise 1
+
+    **Use induction on $n$ to prove Newton's formula: $(a + b)^n = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} a^k b^{n - k}$**.
+
+    Proof
+
+    If $n = 0$, $(a + b)^n = 1$, $\sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} a^k b^{n - k} = 1 \cdot 1 \cdot 1 = 1$, thus equality holds.
+
+    Suppose equality holds for $m$, then: $$\begin{aligned}(a + b)^{m + 1} &= (a + b)(a + b)^m \\ &= (a + b) \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k} \\ &= \sum\limits_{k = 0}^{m} (\begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k}a + \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k}b) \\ &= \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^{k + 1} b^{m - k} + \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m + 1 - k} \\ &= \sum\limits_{k = 1}^{m + 1} \begin{pmatrix} m \\ k - 1 \end{pmatrix} a^{k} b^{m + 1 - k} + \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m + 1 - k} \\ &= \sum\limits_{k = 1}^{m} [(\begin{pmatrix} m \\ k - 1 \end{pmatrix} + \begin{pmatrix} m \\ k \end{pmatrix}) a^{k} b^{m + 1 - k}] \\ &~~~~~~~~~+ \begin{pmatrix} m \\ m + 1 - 1 \end{pmatrix} a^{m + 1}b^{m + 1 - m - 1} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1 - 0} \\ &= \sum\limits_{k = 1}^{m} [\begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}] + \begin{pmatrix} m \\ m \end{pmatrix} a^{m + 1}b^{0} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1} \\ &= \sum\limits_{k = 1}^{m} (\begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}) + \begin{pmatrix} m + 1 \\ m + 1 \end{pmatrix} a^{m + 1}b^{0} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1} \\ &= \sum\limits_{k = 0}^{m + 1} \begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}.\end{aligned}$$ Thus by induction, equality holds for all natural $n$.
+
+    ### Exercise 2
+
+    **Let $n$ be a positive integer. Prove that for any $k \in \lbrace 0, \dots, n \rbrace$, one has $\begin{pmatrix} n \\ k \end{pmatrix} < 2^n$**.
+
+    Proof
+
+    By Newton's formula, $2^n = (1 + 1)^n = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} 1^k 1^{n - k} = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix}$, since each $\begin{pmatrix} n \\ k \end{pmatrix} \ge 1$ by definition, and if $n$ positive, there is more than $1$ term in the summation, clearly $\begin{pmatrix} n \\ k \end{pmatrix} < 2^n$ for each possible $k$.
+
+    ### Exercise 3
+
+    **Use induction to prove the pigeonhole principle: any map from a set with $n + 1$ objects to a set with n objects is not injective**.
+
+    Proof
+
+    If $n = 0$, clearly $1$ object cannot be assigned injectively to $0$ object (I will use 'box' to reduce confusion).
+
+    Assume that any map from a set with $m + 1$ objects to a set with $m$ boxes is not injective, then if we have $m + 2$ objects and $m + 1$ boxes:
+
+    For any map from $m + 2$ objects and $m + 1$ boxes, for convenience and without losing generality, index the boxes from $1$ to $m + 1$.
+
+    If $2$ or more objects are assigned to box No. 1, then clearly this map is not injective; if $1$ object is assigned to box No. 1, then we have $m + 1$ objects and $m$ boxes to be assigned. By assumption, there is no injective map to do so; if $0$ object is assigned to box No. 1, then we have $m + 2$ objects and $m$ boxes to be assigned. By assumption, there is no injective map to assign $m + 1$ objects to $m$ boxes injectively, thus clearly there is no injective map to assign $m + 2 - (m + 1) = 1$ more object.
+
+    Thus by induction, any map from a set with $n + 1$ objects to a set with n objects is not injective.
+    """,
+    column=2, disabled=False, hide_code=False, name="_"
+)
+
+
+app._unparsable_cell(
+    r"""
+    ## Section 0.7
+
+    ### Exercise 0
+
+    **Recalling that $\begin{pmatrix} n \\ k \end{pmatrix} \overset{\text{def}}{=} \frac{n!}{k!(n - k)!}$, prove that for any integers $n \ge k \ge 1$ one has: $\begin{pmatrix} n + 1 \\ k \end{pmatrix} = \begin{pmatrix} n \\ k \end{pmatrix} + \begin{pmatrix} n \\ k-1 \end{pmatrix}$**.
+
+    Proof
+
+    We have $$\begin{aligned}\begin{pmatrix} n + 1 \\ k \end{pmatrix} &\overset{\text{def}}{=} \frac{(n + 1)!}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1)}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 + k - k)}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k) + (n!)k}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k)}{k!(n + 1 - k)!} + \frac{(n)!k}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k)}{k!(n - k)!(n + 1 - k)} + \frac{(n)!k}{(k - 1)!(n - k + 1)!k} \\ &= \frac{(n)!}{k!(n - k)!} + \frac{(n)!}{(k - 1)!(n - k + 1)!} \\ &\overset{\text{def}}{=} \begin{pmatrix} n \\ k \end{pmatrix} + \begin{pmatrix} n \\ k-1 \end{pmatrix}.\end{aligned}$$
+
+    ### Exercise 1
+
+    **Use induction on $n$ to prove Newton's formula: $(a + b)^n = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} a^k b^{n - k}$**.
+
+    Proof
+
+    If $n = 0$, $(a + b)^n = 1$, $\sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} a^k b^{n - k} = 1 \cdot 1 \cdot 1 = 1$, thus equality holds.
+
+    Suppose equality holds for $m$, then: $$\begin{aligned}(a + b)^{m + 1} &= (a + b)(a + b)^m \\ &= (a + b) \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k} \\ &= \sum\limits_{k = 0}^{m} (\begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k}a + \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k}b) \\ &= \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^{k + 1} b^{m - k} + \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m + 1 - k} \\ &= \sum\limits_{k = 1}^{m + 1} \begin{pmatrix} m \\ k - 1 \end{pmatrix} a^{k} b^{m + 1 - k} + \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m + 1 - k} \\ &= \sum\limits_{k = 1}^{m} [(\begin{pmatrix} m \\ k - 1 \end{pmatrix} + \begin{pmatrix} m \\ k \end{pmatrix}) a^{k} b^{m + 1 - k}] \\ &~~~~~~~~~+ \begin{pmatrix} m \\ m + 1 - 1 \end{pmatrix} a^{m + 1}b^{m + 1 - m - 1} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1 - 0} \\ &= \sum\limits_{k = 1}^{m} [\begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}] + \begin{pmatrix} m \\ m \end{pmatrix} a^{m + 1}b^{0} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1} \\ &= \sum\limits_{k = 1}^{m} (\begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}) + \begin{pmatrix} m + 1 \\ m + 1 \end{pmatrix} a^{m + 1}b^{0} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1} \\ &= \sum\limits_{k = 0}^{m + 1} \begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}.\end{aligned}$$ Thus by induction, equality holds for all natural $n$.
+
+    ### Exercise 2
+
+    **Let $n$ be a positive integer. Prove that for any $k \in \lbrace 0, \dots, n \rbrace$, one has $\begin{pmatrix} n \\ k \end{pmatrix} < 2^n$**.
+
+    Proof
+
+    By Newton's formula, $2^n = (1 + 1)^n = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} 1^k 1^{n - k} = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix}$, since each $\begin{pmatrix} n \\ k \end{pmatrix} \ge 1$ by definition, and if $n$ positive, there is more than $1$ term in the summation, clearly $\begin{pmatrix} n \\ k \end{pmatrix} < 2^n$ for each possible $k$.
+
+    ### Exercise 3
+
+    **Use induction to prove the pigeonhole principle: any map from a set with $n + 1$ objects to a set with n objects is not injective**.
+
+    Proof
+
+    If $n = 0$, clearly $1$ object cannot be assigned injectively to $0$ object (I will use 'box' to reduce confusion).
+
+    Assume that any map from a set with $m + 1$ objects to a set with $m$ boxes is not injective, then if we have $m + 2$ objects and $m + 1$ boxes:
+
+    For any map from $m + 2$ objects and $m + 1$ boxes, for convenience and without losing generality, index the boxes from $1$ to $m + 1$.
+
+    If $2$ or more objects are assigned to box No. 1, then clearly this map is not injective; if $1$ object is assigned to box No. 1, then we have $m + 1$ objects and $m$ boxes to be assigned. By assumption, there is no injective map to do so; if $0$ object is assigned to box No. 1, then we have $m + 2$ objects and $m$ boxes to be assigned. By assumption, there is no injective map to assign $m + 1$ objects to $m$ boxes injectively, thus clearly there is no injective map to assign $m + 2 - (m + 1) = 1$ more object.
+
+    Thus by induction, any map from a set with $n + 1$ objects to a set with n objects is not injective.
+    """,
+    column=3, disabled=False, hide_code=False, name="_"
+)
+
+
+app._unparsable_cell(
+    r"""
+    ## Section 0.7
+
+    ### Exercise 0
+
+    **Recalling that $\begin{pmatrix} n \\ k \end{pmatrix} \overset{\text{def}}{=} \frac{n!}{k!(n - k)!}$, prove that for any integers $n \ge k \ge 1$ one has: $\begin{pmatrix} n + 1 \\ k \end{pmatrix} = \begin{pmatrix} n \\ k \end{pmatrix} + \begin{pmatrix} n \\ k-1 \end{pmatrix}$**.
+
+    Proof
+
+    We have $$\begin{aligned}\begin{pmatrix} n + 1 \\ k \end{pmatrix} &\overset{\text{def}}{=} \frac{(n + 1)!}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1)}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 + k - k)}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k) + (n!)k}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k)}{k!(n + 1 - k)!} + \frac{(n)!k}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k)}{k!(n - k)!(n + 1 - k)} + \frac{(n)!k}{(k - 1)!(n - k + 1)!k} \\ &= \frac{(n)!}{k!(n - k)!} + \frac{(n)!}{(k - 1)!(n - k + 1)!} \\ &\overset{\text{def}}{=} \begin{pmatrix} n \\ k \end{pmatrix} + \begin{pmatrix} n \\ k-1 \end{pmatrix}.\end{aligned}$$
+
+    ### Exercise 1
+
+    **Use induction on $n$ to prove Newton's formula: $(a + b)^n = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} a^k b^{n - k}$**.
+
+    Proof
+
+    If $n = 0$, $(a + b)^n = 1$, $\sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} a^k b^{n - k} = 1 \cdot 1 \cdot 1 = 1$, thus equality holds.
+
+    Suppose equality holds for $m$, then: $$\begin{aligned}(a + b)^{m + 1} &= (a + b)(a + b)^m \\ &= (a + b) \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k} \\ &= \sum\limits_{k = 0}^{m} (\begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k}a + \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k}b) \\ &= \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^{k + 1} b^{m - k} + \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m + 1 - k} \\ &= \sum\limits_{k = 1}^{m + 1} \begin{pmatrix} m \\ k - 1 \end{pmatrix} a^{k} b^{m + 1 - k} + \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m + 1 - k} \\ &= \sum\limits_{k = 1}^{m} [(\begin{pmatrix} m \\ k - 1 \end{pmatrix} + \begin{pmatrix} m \\ k \end{pmatrix}) a^{k} b^{m + 1 - k}] \\ &~~~~~~~~~+ \begin{pmatrix} m \\ m + 1 - 1 \end{pmatrix} a^{m + 1}b^{m + 1 - m - 1} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1 - 0} \\ &= \sum\limits_{k = 1}^{m} [\begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}] + \begin{pmatrix} m \\ m \end{pmatrix} a^{m + 1}b^{0} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1} \\ &= \sum\limits_{k = 1}^{m} (\begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}) + \begin{pmatrix} m + 1 \\ m + 1 \end{pmatrix} a^{m + 1}b^{0} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1} \\ &= \sum\limits_{k = 0}^{m + 1} \begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}.\end{aligned}$$ Thus by induction, equality holds for all natural $n$.
+
+    ### Exercise 2
+
+    **Let $n$ be a positive integer. Prove that for any $k \in \lbrace 0, \dots, n \rbrace$, one has $\begin{pmatrix} n \\ k \end{pmatrix} < 2^n$**.
+
+    Proof
+
+    By Newton's formula, $2^n = (1 + 1)^n = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} 1^k 1^{n - k} = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix}$, since each $\begin{pmatrix} n \\ k \end{pmatrix} \ge 1$ by definition, and if $n$ positive, there is more than $1$ term in the summation, clearly $\begin{pmatrix} n \\ k \end{pmatrix} < 2^n$ for each possible $k$.
+
+    ### Exercise 3
+
+    **Use induction to prove the pigeonhole principle: any map from a set with $n + 1$ objects to a set with n objects is not injective**.
+
+    Proof
+
+    If $n = 0$, clearly $1$ object cannot be assigned injectively to $0$ object (I will use 'box' to reduce confusion).
+
+    Assume that any map from a set with $m + 1$ objects to a set with $m$ boxes is not injective, then if we have $m + 2$ objects and $m + 1$ boxes:
+
+    For any map from $m + 2$ objects and $m + 1$ boxes, for convenience and without losing generality, index the boxes from $1$ to $m + 1$.
+
+    If $2$ or more objects are assigned to box No. 1, then clearly this map is not injective; if $1$ object is assigned to box No. 1, then we have $m + 1$ objects and $m$ boxes to be assigned. By assumption, there is no injective map to do so; if $0$ object is assigned to box No. 1, then we have $m + 2$ objects and $m$ boxes to be assigned. By assumption, there is no injective map to assign $m + 1$ objects to $m$ boxes injectively, thus clearly there is no injective map to assign $m + 2 - (m + 1) = 1$ more object.
+
+    Thus by induction, any map from a set with $n + 1$ objects to a set with n objects is not injective.
+    """,
+    column=4, disabled=False, hide_code=False, name="_"
+)
+
+
+app._unparsable_cell(
+    r"""
+    ## Section 0.7
+
+    ### Exercise 0
+
+    **Recalling that $\begin{pmatrix} n \\ k \end{pmatrix} \overset{\text{def}}{=} \frac{n!}{k!(n - k)!}$, prove that for any integers $n \ge k \ge 1$ one has: $\begin{pmatrix} n + 1 \\ k \end{pmatrix} = \begin{pmatrix} n \\ k \end{pmatrix} + \begin{pmatrix} n \\ k-1 \end{pmatrix}$**.
+
+    Proof
+
+    We have $$\begin{aligned}\begin{pmatrix} n + 1 \\ k \end{pmatrix} &\overset{\text{def}}{=} \frac{(n + 1)!}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1)}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 + k - k)}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k) + (n!)k}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k)}{k!(n + 1 - k)!} + \frac{(n)!k}{k!(n + 1 - k)!} \\ &= \frac{(n)!(n + 1 - k)}{k!(n - k)!(n + 1 - k)} + \frac{(n)!k}{(k - 1)!(n - k + 1)!k} \\ &= \frac{(n)!}{k!(n - k)!} + \frac{(n)!}{(k - 1)!(n - k + 1)!} \\ &\overset{\text{def}}{=} \begin{pmatrix} n \\ k \end{pmatrix} + \begin{pmatrix} n \\ k-1 \end{pmatrix}.\end{aligned}$$
+
+    ### Exercise 1
+
+    **Use induction on $n$ to prove Newton's formula: $(a + b)^n = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} a^k b^{n - k}$**.
+
+    Proof
+
+    If $n = 0$, $(a + b)^n = 1$, $\sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} a^k b^{n - k} = 1 \cdot 1 \cdot 1 = 1$, thus equality holds.
+
+    Suppose equality holds for $m$, then: $$\begin{aligned}(a + b)^{m + 1} &= (a + b)(a + b)^m \\ &= (a + b) \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k} \\ &= \sum\limits_{k = 0}^{m} (\begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k}a + \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m - k}b) \\ &= \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^{k + 1} b^{m - k} + \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m + 1 - k} \\ &= \sum\limits_{k = 1}^{m + 1} \begin{pmatrix} m \\ k - 1 \end{pmatrix} a^{k} b^{m + 1 - k} + \sum\limits_{k = 0}^{m} \begin{pmatrix} m \\ k \end{pmatrix} a^k b^{m + 1 - k} \\ &= \sum\limits_{k = 1}^{m} [(\begin{pmatrix} m \\ k - 1 \end{pmatrix} + \begin{pmatrix} m \\ k \end{pmatrix}) a^{k} b^{m + 1 - k}] \\ &~~~~~~~~~+ \begin{pmatrix} m \\ m + 1 - 1 \end{pmatrix} a^{m + 1}b^{m + 1 - m - 1} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1 - 0} \\ &= \sum\limits_{k = 1}^{m} [\begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}] + \begin{pmatrix} m \\ m \end{pmatrix} a^{m + 1}b^{0} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1} \\ &= \sum\limits_{k = 1}^{m} (\begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}) + \begin{pmatrix} m + 1 \\ m + 1 \end{pmatrix} a^{m + 1}b^{0} + \begin{pmatrix} m \\ 0 \end{pmatrix} a^0 b^{m + 1} \\ &= \sum\limits_{k = 0}^{m + 1} \begin{pmatrix} m + 1 \\ k \end{pmatrix} a^{k} b^{m + 1 - k}.\end{aligned}$$ Thus by induction, equality holds for all natural $n$.
+
+    ### Exercise 2
+
+    **Let $n$ be a positive integer. Prove that for any $k \in \lbrace 0, \dots, n \rbrace$, one has $\begin{pmatrix} n \\ k \end{pmatrix} < 2^n$**.
+
+    Proof
+
+    By Newton's formula, $2^n = (1 + 1)^n = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix} 1^k 1^{n - k} = \sum\limits_{k = 0}^{n} \begin{pmatrix} n \\ k \end{pmatrix}$, since each $\begin{pmatrix} n \\ k \end{pmatrix} \ge 1$ by definition, and if $n$ positive, there is more than $1$ term in the summation, clearly $\begin{pmatrix} n \\ k \end{pmatrix} < 2^n$ for each possible $k$.
+
+    ### Exercise 3
+
+    **Use induction to prove the pigeonhole principle: any map from a set with $n + 1$ objects to a set with n objects is not injective**.
+
+    Proof
+
+    If $n = 0$, clearly $1$ object cannot be assigned injectively to $0$ object (I will use 'box' to reduce confusion).
+
+    Assume that any map from a set with $m + 1$ objects to a set with $m$ boxes is not injective, then if we have $m + 2$ objects and $m + 1$ boxes:
+
+    For any map from $m + 2$ objects and $m + 1$ boxes, for convenience and without losing generality, index the boxes from $1$ to $m + 1$.
+
+    If $2$ or more objects are assigned to box No. 1, then clearly this map is not injective; if $1$ object is assigned to box No. 1, then we have $m + 1$ objects and $m$ boxes to be assigned. By assumption, there is no injective map to do so; if $0$ object is assigned to box No. 1, then we have $m + 2$ objects and $m$ boxes to be assigned. By assumption, there is no injective map to assign $m + 1$ objects to $m$ boxes injectively, thus clearly there is no injective map to assign $m + 2 - (m + 1) = 1$ more object.
+
+    Thus by induction, any map from a set with $n + 1$ objects to a set with n objects is not injective.
+    """,
+    column=5, disabled=False, hide_code=False, name="_"
+)
+
+
 if __name__ == "__main__":
     app.run()
