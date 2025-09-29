72b4b1481cba846fb727e4b49248cf6f3852da0a
Zedan Liu <zedan.liu@icloud.com>
2025-09-29 09:51:29 -0400

    0929

--- notebooks/SQL_20250926.py
+++ notebooks/SQL_20250926.py
@@ -26,17 +26,81 @@ def _(mo):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r""" """)
+    mo.md(
+        r"""
+    ### LeetCode 2783
+
+    Table: Flights
+
+    | Column Name | Type |
+    |-------------|------|
+    | flight_id   | int  |
+    | capacity    | int  |
+
+    flight_id is the column with unique values for this table. Each row of this table contains flight id and its capacity.
+
+    Table: Passengers
+
+    | Column Name  | Type |
+    |--------------|------|
+    | passenger_id | int  |
+    | flight_id    | int  |
+
+    passenger_id is the column with unique values for this table. Each row of this table contains passenger id and flight id. Passengers book tickets for flights in advance. If a passenger books a ticket for a flight and there are still empty seats available on the flight, the passenger ticket will be confirmed. However, the passenger will be on a waitlist if the flight is already at full capacity.
+
+    Write a solution to report the number of passengers who successfully booked a flight (got a seat) and the number of passengers who are on the waitlist for each flight.
+
+    Return the result table ordered by flight_id in ascending order.
+    """
+    )
     return
 
 
 @app.cell
 def _():
+    """
+    WITH number_passenger AS (
+        SELECT
+            flight_id,
+            COUNT(DISTINCT passenger_id) AS num_passenger
+        FROM
+            Passengers
+        GROUP BY
+            1
+    )
+    SELECT
+        flight_id,
+        LEAST(COALESCE(num_passenger,0), capacity) AS booked_cnt,
+        GREATEST(COALESCE(num_passenger,0) - capacity, 0) AS waitlist_cnt
+    FROM
+        Flights
+            LEFT JOIN number_passenger USING (flight_id)
+    ORDER BY
+        1;
+    """
     return
 
 
 @app.cell
 def _():
+    import pandas as pd
+
+
+    def waitlist_analysis(
+        flights: pd.DataFrame, passengers: pd.DataFrame
+    ) -> pd.DataFrame:
+        number_passenger = passengers.groupby("flight_id", as_index=False)[
+            "passenger_id"
+        ].nunique()
+        df = pd.merge(
+            flights, number_passenger, how="left", on="flight_id"
+        ).fillna(0)
+        df["booked_cnt"] = df[["capacity", "passenger_id"]].min(axis=1)
+        df["waitlist_cnt"] = (df["passenger_id"] - df["capacity"]).clip(lower=0)
+        df = df[["flight_id", "booked_cnt", "waitlist_cnt"]].sort_values(
+            "flight_id"
+        )
+        return df
     return
 
 
---
+++ notebooks/SQL_20250929.py
@@ -0,0 +1,83 @@
+import marimo
+
+__generated_with = "0.15.4"
+app = marimo.App()
+
+
+@app.cell(hide_code=True)
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    nav_menu = mo.nav_menu(
+        {
+            "/index.html": f"{mo.icon('lucide:home')} Home",
+            "/notebooks/SQL_20250926.html": f"{mo.icon('lucide:arrow-big-left')} Last Day",
+            "/notebooks/SQL_20250930.html": f"{mo.icon('lucide:arrow-big-right')} Next Day",
+        },
+        orientation="horizontal",
+    )
+    nav_menu
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    ### LeetCode 2738
+
+    Table: Files
+
+    | Column Name | Type    |
+    |-- ----------|---------|
+    | file_name   | varchar |
+    | content     | text    |
+
+    file_name is the column with unique values of this table. Each row contains file_name and the content of that file.
+
+    Write a solution to find the number of files that have at least one occurrence of the words 'bull' and 'bear' as a standalone word, respectively, disregarding any instances where it appears without space on either side (e.g. 'bullet', 'bears', 'bull.', or 'bear' at the beginning or end of a sentence will not be considered) 
+
+    Return the word 'bull' and 'bear' along with the corresponding number of occurrences in any order.
+    """
+    )
+    return
+
+
+@app.cell
+def _():
+    """
+    SELECT
+        'bull' AS word,
+        COUNT(1) FILTER (WHERE content LIKE '% bull %') AS count
+    FROM
+        Files
+    UNION
+    SELECT
+        'bear' AS word,
+        COUNT(1) FILTER (WHERE content LIKE '% bear %') AS count
+    FROM
+        Files
+    """
+    return
+
+
+@app.cell
+def _():
+    import pandas as pd
+
+
+    def count_occurrences(files: pd.DataFrame) -> pd.DataFrame:
+        bullcnt = files["content"].str.contains(" bull ").sum()
+        bearcnt = files["content"].str.contains(" bear ").sum()
+        return pd.DataFrame(
+            {"word": ["bull", "bear"], "count": [bullcnt, bearcnt]}
+        )
+    return
+
+
+if __name__ == "__main__":
+    app.run()
