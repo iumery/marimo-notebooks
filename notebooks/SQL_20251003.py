87d1126f7bccba1a6f1009ed089e9ef4ff04ef6d
Zedan Liu <zedan.liu@icloud.com>
2025-10-03 11:13:03 -0400

    1003

--- notebooks/SQL_20251003.py
+++ notebooks/SQL_20251003.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.15.4"
+__generated_with = "0.16.5"
 app = marimo.App()
 
 
@@ -26,17 +26,73 @@ def _(mo):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r""" """)
+    mo.md(
+        r"""
+    ### LeetCode 2922
+
+    Table: Users
+
+    | Column Name    | Type    |
+    |----------------|---------|
+    | seller_id      | int     |
+    | join_date      | date    |
+    | favorite_brand | varchar |
+
+    seller_id is column of unique values for this table. This table contains seller id, join date, and favorite brand of sellers.
+
+    Table: Items
+
+    | Column Name   | Type    |
+    |---------------|---------|
+    | item_id       | int     |
+    | item_brand    | varchar |
+
+    item_id is the column of unique values for this table. This table contains item id and item brand.
+
+    Table: Orders
+
+    | Column Name   | Type    |
+    |---------------|---------|
+    | order_id      | int     |
+    | order_date    | date    |
+    | item_id       | int     |
+    | seller_id     | int     |
+
+    order_id is the column of unique values for this table. item_id is a foreign key to the Items table. seller_id is a foreign key to the Users table. This table contains order id, order date, item id and seller id.
+
+    Write a solution to find the top seller who has sold the highest number of unique items with a different brand than their favorite brand. If there are multiple sellers with the same highest count, return all of them.
+
+    Return the result table ordered by seller_id in ascending order.
+    """
+    )
     return
 
 
 @app.cell
 def _():
-    return
-
-
-@app.cell
-def _():
+    """
+    WITH sell_unique AS (
+        SELECT
+            seller_id,
+            COUNT(DISTINCT item_id) AS num_items
+        FROM
+            Orders
+                LEFT JOIN Users USING (seller_id)
+                LEFT JOIN Items USING (item_id)
+        WHERE
+            favorite_brand <> item_brand
+        GROUP BY
+            1
+    )
+    SELECT
+        *
+    FROM
+        sell_unique
+    WHERE
+        num_items = (SELECT MAX(num_items) FROM sell_unique)
+    ORDER BY
+        seller_id;
+    """
     return
 
 
