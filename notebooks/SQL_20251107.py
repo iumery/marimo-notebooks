de03e5a66ba26c7b32f72a3be4a8ad7c0a6550e5
Zedan Liu <zedan.liu@icloud.com>
2025-11-04 11:31:06 -0500

    1104

--- notebooks/SQL_20251107.py
+++ notebooks/SQL_20251107.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.16.5"
+__generated_with = "0.17.4"
 app = marimo.App()
 
 
@@ -26,17 +26,65 @@ def _(mo):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r""" """)
+    mo.md(r"""
+    ### LeetCode 3060
+
+    Table: Sessions
+
+    | Column Name   | Type     |
+    |---------------|----------|
+    | user_id       | int      |
+    | session_start | datetime |
+    | session_end   | datetime |
+    | session_id    | int      |
+    | session_type  | enum     |
+
+    session_id is column of unique values for this table. session_type is an ENUM (category) type of (Viewer, Streamer). This table contains user id, session start, session end, session id and session type.
+
+    Write a solution to find the the users who have had at least two session of the same type (either 'Viewer' or 'Streamer') with a maximum gap of 12 hours between sessions.
+
+    Return the result table ordered by user_id in ascending order.
+    """)
     return
 
 
 @app.cell
 def _():
+    """
+    SELECT DISTINCT
+        s1.user_id
+    FROM
+        Sessions s1
+            JOIN Sessions s2 ON s1.user_id = s2.user_id
+                AND s1.session_type = s2.session_type
+                AND s1.session_start < s2.session_start
+                AND s2.session_start - s1.session_end <= INTERVAL '12 hour'
+    ORDER BY
+        1;
+    """
     return
 
 
 @app.cell
 def _():
+    import pandas as pd
+
+
+    def user_activities(sessions: pd.DataFrame) -> pd.DataFrame:
+        df = sessions.sort_values(by=["user_id", "session_type", "session_start"])
+        df["next_session_start"] = df.groupby(["user_id", "session_type"])[
+            "session_start"
+        ].shift(-1)
+        df["diff_hours"] = (
+            df["next_session_start"] - df["session_end"]
+        ).dt.total_seconds() / 3600
+        df = (
+            df[df["diff_hours"] <= 12][["user_id"]]
+            .drop_duplicates()
+            .sort_values(by="user_id")
+        )
+
+        return df
     return
 
 
